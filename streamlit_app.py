b99f56daa80460477045cfb20086b0dfb07e295e
Adam Clouston <124040923+ACloustonEFC@users.noreply.github.com>
2023-01-31 10:44:59 +0000

    Update streamlit_app.py
    imported pandas
    imported csv
    displayed csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title("EFC Snowflake Test")
 
 streamlit.header("Test")
 streamlit.text("Text text text")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
